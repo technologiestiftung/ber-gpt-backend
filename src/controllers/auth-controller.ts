ceb77318dbc8ae30e21fd61f390df6f38c38937b
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-07-18 16:28:29 +0200

    fix: always use secure cookies

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -17,7 +17,7 @@ export const login = async (req: Request, res: Response) => {
     // Set a cookie with the session token
     res.cookie("sb:token", data.session?.access_token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+      secure: true,
       maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
     });
 
