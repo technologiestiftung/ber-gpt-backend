bf5c996a3fe22d8ef9ef4f4302446db781dee950
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-07-22 13:19:50 +0200

    chore: update deps

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -5,7 +5,7 @@ const corsMiddleware = (config: Config) => {
   const corsOptions = {
     origin: config.corsAllowedOrigin,
     methods: "GET,POST",
-    allowedHeaders: "Content-Type,Authorization",
+    allowedHeaders: "Content-Type,Authorization,x-api-key",
     optionsSuccessStatus: 200,
   };
 
