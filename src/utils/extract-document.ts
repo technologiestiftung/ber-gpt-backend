6d888180f9ddcee69dd5d0a866f576f346a94c24
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-07-22 10:29:38 +0200

    fix: imports

--- src/utils/extract-document.ts
+++ src/utils/extract-document.ts
@@ -1,13 +1,8 @@
-import { get_encoding } from "@dqbd/tiktoken";
-
-export const enc = get_encoding("cl100k_base");
-
 export const extract = async (documentFileBuffer: Buffer): Promise<string> => {
   // @ts-ignore
   const pdf2md = (await import("@opendocsg/pdf2md")).default;
   const buffer = Buffer.from(documentFileBuffer);
   const uint8Array = new Uint8Array(buffer);
   const mdText = await pdf2md(uint8Array);
-
   return mdText;
 };
