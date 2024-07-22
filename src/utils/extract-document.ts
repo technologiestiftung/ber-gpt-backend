da0f47599107b2817bd1fdfc376fa0158c8bb301
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2024-07-22 10:42:48 +0200

    feat: use pdf-parse

--- src/utils/extract-document.ts
+++ src/utils/extract-document.ts
@@ -1,8 +1,7 @@
+import pdf from "pdf-parse";
+
 export const extract = async (documentFileBuffer: Buffer): Promise<string> => {
-  // @ts-ignore
-  const pdf2md = (await import("@opendocsg/pdf2md")).default;
   const buffer = Buffer.from(documentFileBuffer);
-  const uint8Array = new Uint8Array(buffer);
-  const mdText = await pdf2md(uint8Array);
-  return mdText;
+  const mdText = await pdf(buffer);
+  return mdText.text;
 };
